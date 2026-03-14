3a1203efaf58f9f1b7aa1b5bc40527cacd3eec24
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-14 04:16:53 +0300

    add ExperimentValidator

--- src/main/java/domain/Experiment.java
+++ src/main/java/domain/Experiment.java
@@ -6,11 +6,11 @@ public final class Experiment {
     // Уникальный номер эксперимента. Программа назначает сама.
     private long id;
     // Название эксперимента. Нельзя пустое. До 128 символов.
-    private String name;
+    public String name;
     // Описание (кратко “что делаем”). Можно пусто. До 512 символов.
-    private String description;
+    public String description;
     // Кто создал (логин). На ранних этапах можно "SYSTEM".
-    private String ownerUsername;
+    public String ownerUsername;
     // Когда создан. Программа ставит автоматически.
     private Instant createdAt;
     // Когда изменяли. Программа обновляет автоматически.
--- src/main/java/validation/ExperimentValidator.java
+++ src/main/java/validation/ExperimentValidator.java
@@ -1,4 +1,24 @@
 package validation;
 
+import domain.Experiment;
+
 public class ExperimentValidator {
+    public static void validate(Experiment exp) {
+
+        if (exp.name == null) {
+            throw new ValidationException("Experiment name can't be empty");
+        }
+
+        if (exp.name.length() >= 128) {
+            throw new ValidationException("Experiment name too long.");
+        }
+
+        if (exp.description != null && exp.description.length() > 512) {
+            throw new ValidationException("Description too long");
+        }
+
+        if (exp.ownerUsername == null) {
+            throw new ValidationException("Owner username cannot be empty");
+        }
+    }
 }
